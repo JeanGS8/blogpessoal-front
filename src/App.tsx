6b00678a44312c4859683078a5ad10721052f214
Jean <jean118877@hotmail.com>
2022-12-01 15:26:29 -0300

    feat: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
-import logo from './logo.svg';
 import './App.css';
 import { Home } from './pages/home/Home';
 import { Navbar } from './componentes/estaticos/navbar/Navbar';
 import { Footer } from './componentes/estaticos/footer/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
+    <Router>
       <Navbar/>
-      <Home/>
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/home' element={<Home/>}/>
+      </Routes>
+      
       <Footer/>
+    </Router>
     </>
   );
 }
