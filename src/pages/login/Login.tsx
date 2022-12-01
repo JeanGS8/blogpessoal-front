b97fdc783db8f771bf553ec909d261e7e4627e5b
Jean Gomes <jean118877@hotmail.com>
2022-12-01 20:41:13 -0300

    feat: Login incompleto

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,20 @@
+import { Grid } from '@material-ui/core';
+import { Box } from '@mui/material';
 import React from 'react';
 import './Login.css';
 
 function Login(){
   return(
     <>
+      <Grid container>
+        <Grid item xs={6}>
+          <Box>
+            <form action=""></form>
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+        </Grid>
+      </Grid>
     </>
   );
 }
