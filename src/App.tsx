9c3e7d1bb7b6a69da46ac2808d1d244f56a5f493
Jean Gomes <jean118877@hotmail.com>
2022-12-02 15:34:42 -0300

    feat: aula 15

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,18 @@ import { Home } from './pages/home/Home';
 import { Navbar } from './componentes/estaticos/navbar/Navbar';
 import { Footer } from './componentes/estaticos/footer/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Login } from './pages/login/Login';
 
 function App() {
   return (
     <>
     <Router>
       <Navbar/>
-
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/login' element={<Login/>}/>
         <Route path='/home' element={<Home/>}/>
       </Routes>
-      
       <Footer/>
     </Router>
     </>
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,5 @@
-import { Grid } from '@material-ui/core';
-import { Box } from '@mui/material';
+import { Grid, Typography } from '@material-ui/core';
+import { Box, TextField } from '@mui/material';
 import React from 'react';
 import './Login.css';
 
@@ -9,11 +9,16 @@ function Login(){
       <Grid container>
         <Grid item xs={6}>
           <Box>
-            <form action=""></form>
+            <form>
+              <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{fontWeight: 'bold'}}>
+                Entrar
+              </Typography>
+              <TextField id="usuario" label="usuario" variant="outlined" name="usuario" margin="normal" fullWidth/>
+              <TextField id="senha" label="senha" variant="outlined" name="senha" margin="normal" type="password" fullWidth/>
+            </form>
           </Box>
         </Grid>
-        <Grid item xs={6}>
-        </Grid>
+        <Grid item xs={6}></Grid>
       </Grid>
     </>
   );
