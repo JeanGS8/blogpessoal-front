2d40b4760f642b985b69cb7ce37f2c991c9666b1
Jean <jean118877@hotmail.com>
2022-12-06 16:42:16 -0300

    aula 32

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import User from '../models/User';
 
 // link da API
 export const api = axios.create({
@@ -9,7 +10,7 @@ export const api = axios.create({
 // dados = enviando os dados necessários para cadastrar
 // resposta = retorno da API
 // setDado = função que filtra a resposta e pega o retorno da API (id, nome, usuario, senha, foto)
-export const cadastroUsuario = async(url: any, dados: any, setDado: any) => {
+export const cadastroUsuario = async(url: string, dados: {nome: string, usuario: string, senha: string}, setDado: React.Dispatch<React.SetStateAction<User>>) => {
     const resposta = await api.post(url, dados)
     setDado(resposta.data)
 }
@@ -18,7 +19,7 @@ export const cadastroUsuario = async(url: any, dados: any, setDado: any) => {
 // dados = dados necessários para logar
 // resposta = retorno da API
 // setDado = função que filtra a resposta e pega o token
-export const login = async(url: any, dados: any, setDado: any) => {
+export const login = async(url: string, dados: {usuario: string, senha: string}, setDado: React.Dispatch<string>) => {
     const resposta = await api.post(url, dados)
     setDado(resposta.data.token)
 }
@@ -27,7 +28,26 @@ export const login = async(url: any, dados: any, setDado: any) => {
 // header = enviando o token para a API poder identificar o usuário
 // resposta = retorno da API
 // setDado = função que filtra a resposta e pega o retorno da api (postagem: id, titulo, texto, tema || tema: id, descricao)
-export const busca = async(url: any, setDado: any, header: any) => {
+export const busca = async(url: string, setDado: any, header: any) => {
     const resposta = await api.get(url, header)
     setDado(resposta.data)
+}
+
+export const buscaId = async(url: string, setDado: any, header: any) => {
+    const resposta = await api.get(url, header)
+    setDado(resposta.data)
+}
+
+export const post = async(url: string, dados: any, setDado: any, header: any) => {
+    const resposta = await api.post(url, dados, header)
+    setDado(resposta.data)
+}
+
+export const put = async(url: string, dados: any, setDado: any, header: any) => {
+    const resposta = await api.put(url, dados, header)
+    setDado(resposta.data)
+}
+
+export const deleteId = async(url: string, header: any) => {
+    const resposta = await api.delete(url, header)
 }
